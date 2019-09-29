611ef7590aebce5f70ce53515732ba25c2126157
r_takaishi <ryo.takaishi.0@gmail.com>
2019-09-29 17:31:13 +0900

    use promptui

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/manifoldco/promptui"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"os"
 	"path/filepath"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	"strings"
 )
 
 func main() {
@@ -38,14 +40,21 @@ func run() error {
 		return err
 	}
 
-	pods, err := clientset.CoreV1().Pods("kube-system").List(metav1.ListOptions{})
+	pod, err := selectPod(clientset)
 	if err != nil {
 		return err
 	}
-	for _, pod := range pods.Items {
-		fmt.Println(pod.Name)
+
+	actionPrompt := promptui.Select{
+		Label: "actions",
+		Items: []string{"get", "describe"},
 	}
 
+	_, actionName, err := actionPrompt.Run()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("kubectl %s %s\n", actionName, pod.Name)
 	return nil
 }
 
@@ -54,4 +63,43 @@ func homeDir() string {
 		return h
 	}
 	return os.Getenv("USERPROFILE") // windows
+}
+
+func selectPod(clientset *kubernetes.Clientset) (*corev1.Pod, error) {
+	pods, err := clientset.CoreV1().Pods("kube-system").List(metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   "> {{ .Name | cyan }}",
+		Inactive: "  {{ .Name | cyan }}",
+		Selected: "  {{ .Name | red | cyan }}",
+		Details: `
+--------- Pepper ----------
+{{ "Name:" | faint }}	{{ .Name }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		pepper := pods.Items[index]
+		name := strings.Replace(strings.ToLower(pepper.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Pods",
+		Items:     pods.Items,
+		Searcher:  searcher,
+		Templates: templates,
+	}
+
+	i, _, err := prompt.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pods.Items[i], nil
 }
